09ce5049cdd16ac7fdeeb54d592e03c42f0789dd
Vertika Jain <vertika.jain@digi-corp.com>
2020-06-17 23:35:35 +0530

    first commit

---
+++ stringReverse.js
@@ -0,0 +1,16 @@
+// method 1: using for loop
+solution = (str) => {
+    let str1 = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        str1 += str[i];
+    }
+    return str1;
+}
+
+// method 2: using built-in methods
+solution = (str) => {
+    return str.split('').reverse().join('');
+}
+
+let result = solution("qwerty");
+console.log(result);
