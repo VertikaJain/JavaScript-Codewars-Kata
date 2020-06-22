6003b3e374d61902e75ddd1e0532dd96212863ce
Vertika Jain <vertikajain96@gmail.com>
2020-06-23 02:48:00 +0530

    Open or Senior member

---
+++ categorizeNewMember.js
@@ -0,0 +1,9 @@
+// senior: >=55 age && handicap >7
+function openOrSenior(data) {
+    let arr = data.map((item) => {
+        return ((item[0] >= 55 && item[1] > 7) ? "Senior" : "Open")
+    });
+    return arr;
+}
+
+openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]);
